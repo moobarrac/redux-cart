09e3ba205e8ac26f47e2be1adc911ec57c8e8db6
moobarrac <wahabajibola01@gmail.com>
2022-12-26 09:25:49 +0100

    actions

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+export const CLEAR_CART = 'CLEAR_CART';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const INCREASE_ITEM = 'INCREASE_ITEM';
+export const DECREASE_ITEM = 'DECREASE_ITEM';
+export const GET_TOTALS = 'GET_TOTALS';
